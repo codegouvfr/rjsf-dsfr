2b50a17528516e31ae9310a285f8081cb3773ae1
Emile Rolley <emile.rolley@tuta.io>
2024-07-11 16:21:38 +0200

    ci: debuging

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base needs to be changed for links to work in GitHub pages
-  base: process.env.NODE_ENV === 'production' ? '.' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   server: {
     port: 8080,
     open: process.env.NODE_ENV !== 'production',
